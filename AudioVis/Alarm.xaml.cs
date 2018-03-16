acb152939357256c8f105ce18d6451d29a4c532c
vbalabanov <Viacheslav.Balabanov@logianalytics.com>
2018-03-16 15:38:53 +0200

    Sunshine/Sunset alarms first implementation

---
+++ AudioVis/Alarm.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
+
+namespace AudioVis
+{
+    [ValueConversion(typeof(bool?), typeof(bool?))]
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value==null) return null;
+            return !((bool)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    /// <summary>
+    /// Interaction logic for Alarms.xaml
+    /// </summary>
+    public partial class Alarm : UserControl, INotifyPropertyChanged
+    {
+        private string _caption;
+        private bool _isSunSet;
+        private double _currentMultiplierB = 1;
+        private double _currentMultiplierY = 1;
+
+        public bool IsSunSet
+        {
+            get => _isSunSet; set
+            {
+                _isSunSet = value;
+                OnPropertyChanged("IsSunSet");
+            }
+        }
+
+        public string Caption
+        {
+            get => _caption; set
+            {
+                _caption = value;
+                OnPropertyChanged("Caption");
+            }
+        }
+
+        DispatcherTimer start_t;
+        DispatcherTimer modifyTimer;
+        public double CurrentMultiplierB
+        {
+            get => _currentMultiplierB; set
+            {
+                _currentMultiplierB = value;
+                OnPropertyChanged("CurrentMultiplierB");
+            }
+        }
+        public double CurrentMultiplierY
+        {
+            get => _currentMultiplierY; set
+            {
+                _currentMultiplierY = value;
+                OnPropertyChanged("CurrentMultiplierY");
+            }
+        }
+        public Alarm()
+        {
+            InitializeComponent();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void IsEnabled_cb_Checked(object sender, RoutedEventArgs e)
+        {
+            if (interval_tp.SelectedTime == null || interval_tp.SelectedTime.Equals(TimeSpan.Zero) || end_tp.SelectedTime == null)
+                return;
+            TimeSpan currTime = DateTime.Now.TimeOfDay;
+            TimeSpan? startTime = end_tp.SelectedTime - interval_tp.SelectedTime;
+            TimeSpan? delta = startTime - currTime;
+            if (delta.Value<TimeSpan.Zero)
+            {
+                MessageBox.Show("Error in setting alarm time!");
+                return;
+            }
+            CurrentMultiplierB = CurrentMultiplierY = Convert.ToDouble(IsSunSet);
+            start_t = new DispatcherTimer();
+            start_t.Interval = delta.Value;
+            start_t.Tick += Start_t_Tick;
+            start_t.Start();
+        }
+
+        private void Start_t_Tick(object sender, EventArgs e)
+        {
+            (sender as DispatcherTimer).Stop();
+            ushort millsPerPercent = (ushort)Math.Ceiling(interval_tp.SelectedTime.Value.TotalMilliseconds / 200);//100 for Blue and 100 for Yellow
+            modifyTimer = new DispatcherTimer();
+            modifyTimer.Interval = new TimeSpan(millsPerPercent*TimeSpan.TicksPerMillisecond);
+            modifyTimer.Tick += ModifyTimer_Tick;
+            modifyTimer.Start();
+        }
+
+        private void ModifyTimer_Tick(object sender, EventArgs e)
+        {
+            //bool changeB = !(CurrentMultiplierY < 1) || (IsSunSet && CurrentMultiplierB > 0);
+            bool changeB = IsSunSet && (CurrentMultiplierB > 0) || !IsSunSet && !(CurrentMultiplierY < 1);
+            double amount = (-1) * Convert.ToInt16(IsSunSet) * 0.01;
+            CurrentMultiplierB = Math.Round(CurrentMultiplierB + Convert.ToInt16(changeB) * amount, 2);
+            CurrentMultiplierY = Math.Round(CurrentMultiplierY + Convert.ToInt16(!changeB) * amount, 2);
+            if (((CurrentMultiplierB + CurrentMultiplierY)<=0 && IsSunSet) || (CurrentMultiplierB + CurrentMultiplierY)>=2 && !IsSunSet)
+            {
+                (sender as DispatcherTimer).Stop();
+            }
+        }
+
+        private void IsEnabled_cb_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CurrentMultiplierB = CurrentMultiplierY = 1;
+            start_t?.Stop();
+            modifyTimer?.Stop();
+        }
+    }
+}
--- AudioVis/MainWindow.xaml.cs
+++ AudioVis/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ArduinoOutput;
 using MahApps.Metro.Controls;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AudioVis
@@ -8,8 +9,55 @@ namespace AudioVis
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : MetroWindow
     {
+        internal class ChannelValues
+        {
+            internal ushort B;
+            internal ushort Y;
+        }
+        internal class ComputedValues
+        {
+            internal bool twoChannels;
+            internal ChannelValues L;
+            internal ChannelValues R;
+            internal bool valuesModified;
+            internal void ModifyValues(Alarm alarm)
+            {
+                double BMult = alarm.CurrentMultiplierB;
+                double YMult = alarm.CurrentMultiplierY;
+                L.B = (ushort)(L.B * BMult);
+                L.Y = (ushort)(L.Y * YMult);
+                if (twoChannels)
+                {
+                    R.B = (ushort)(R.B * BMult);
+                    R.Y = (ushort)(R.Y * YMult);
+                }
+                valuesModified = true;
+            }
+            internal ComputedValues(FFTDependencyWrapper wrapper)
+            {
+                L = new ChannelValues { B = wrapper.BlueLight.Values[0], Y = wrapper.YellowLight.Values[0] };
+                if (wrapper.TwoChannels)
+                {
+                    twoChannels = true;
+                    R = new ChannelValues { B = wrapper.BlueLight.Values[1], Y = wrapper.YellowLight.Values[1] };
+                }
+            }
+            internal LinkedList<Packet> GetPackets()
+            {
+                LinkedList<Packet> packets = new LinkedList<Packet>();
+                packets.AddLast(new Packet { channelData = (ChannelData.Left | ChannelData.LowFreq), Brightness = (byte)L.B });
+                packets.AddLast(new Packet { channelData = (ChannelData.Left | ChannelData.HighFreq), Brightness = (byte)L.Y });
+                if (twoChannels)
+                {
+                    packets.AddLast(new Packet { channelData = (ChannelData.Right | ChannelData.LowFreq), Brightness = (byte)R.B });
+                    packets.AddLast(new Packet { channelData = (ChannelData.Right | ChannelData.HighFreq), Brightness = (byte)R.Y });
+                }
+                return packets;
+            }
+        }
         public FFTDependencyWrapper FftAndColorsSource { get; set; }
 
         public ArduinoSender ArduinoBridge { get; set; }
@@ -29,19 +77,28 @@ namespace AudioVis
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            FftAndColorsSource.processFFT();
-            Left_v.AddValues(FftAndColorsSource.BlueLight.Values[0], FftAndColorsSource.YellowLight.Values[0]);
-            ArduinoBridge.SendValue(new Packet {channelData = (ChannelData.Left | ChannelData.LowFreq), Brightness = (byte)FftAndColorsSource.BlueLight.Values[0] });
-            ArduinoBridge.SendValue(new Packet { channelData = (ChannelData.Left | ChannelData.HighFreq), Brightness = (byte)FftAndColorsSource.YellowLight.Values[0] });
+            var values = getFinalValues(FftAndColorsSource);
+            Left_v.AddValues(values.L.B, values.L.Y);
             if (FftAndColorsSource.TwoChannels)
             {
-                Right_v.AddValues(FftAndColorsSource.BlueLight.Values[1], FftAndColorsSource.YellowLight.Values[1]);
-                ArduinoBridge.SendValue(new Packet { channelData = (ChannelData.Right | ChannelData.LowFreq), Brightness = (byte)FftAndColorsSource.BlueLight.Values[1] });
-                ArduinoBridge.SendValue(new Packet { channelData = (ChannelData.Right | ChannelData.HighFreq), Brightness = (byte)FftAndColorsSource.YellowLight.Values[1] });
+                Right_v.AddValues(values.R.B, values.R.Y);
+            }
+            foreach (var packet in values.GetPackets())
+            {
+                ArduinoBridge.SendValue(packet);
             }
         }
 
-        
+        private ComputedValues getFinalValues(FFTDependencyWrapper wrapper)
+        {
+            wrapper.processFFT();
+            ComputedValues vals = new ComputedValues(wrapper);
+            if (Sunset_alm.IsEnabled_cb.IsChecked == true)
+            {
+                vals.ModifyValues(Sunset_alm);
+            }
+            return vals;
+        }
 
         private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
