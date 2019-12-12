2e9b2164c1ba9ec2f823215d5980845245933a41
lek <lekarsenten@gmail.com>
2019-12-12 03:18:16 +0200

    Metro update, other CT calculation

--- ArduinoOutput/HueOutput.cs
+++ ArduinoOutput/HueOutput.cs
@@ -12,7 +12,7 @@ namespace OutputBridges
     public class HueSender : PacketSender
     {
         private const string apiVarFormattingString = @"api/k1gK3p0BkT2NAGVYYzCtQkZHTCEiuSTEUnHtmT08/lights/{0}/state";
-        private const string url = @"http://192.168.1.242/";
+        private const string url = @"http://192.168.0.175/";
         private const string commandFormattingString = "{{\"bri\": {0}, \"transitiontime\": 0, \"ct\": {1}}}";
         private RestClient client = new RestClient(url);
 
@@ -20,9 +20,10 @@ namespace OutputBridges
         public override bool SendValues(ChannelValues values)
         {
             //var client = new RestClient(url);
-            var request = new RestRequest(String.Format(apiVarFormattingString, values.isRightChannel?2:4), Method.PUT);
+            var request = new RestRequest(String.Format(apiVarFormattingString, values.isRightChannel?2:6), Method.PUT);
             request.RequestFormat = DataFormat.Json;
-            string jsonString = String.Format(commandFormattingString, ((values.Y + values.B)/2) - 1, getCT(values));
+            //string jsonString = String.Format(commandFormattingString, ((values.Y + values.B)/2) - 1, getCT(values));
+            string jsonString = String.Format(commandFormattingString, Math.Max(values.Y, values.B), getCT(values));
             request.AddParameter("text/json", jsonString, ParameterType.RequestBody);
             var result = client.Execute(request);
             if (result.StatusCode == System.Net.HttpStatusCode.OK)
@@ -39,7 +40,9 @@ namespace OutputBridges
 
         private double getCT(ChannelValues values)
         {
-            return 326 + 173 * (values.Y - values.B) / 255; /*210*/  /*60*/ /*mid=135*/
+            //return 326 + 173 * (values.Y - values.B) / 255; /*210*/  /*60*/ /*mid=135*/
+            return Convert.ToUInt16(153 + 347 * (Math.Atan2(values.Y, values.B) * (180 / Math.PI)) / 90); /*210*/  /*60*/ /*mid=135*/
+
         }
     }
 }
